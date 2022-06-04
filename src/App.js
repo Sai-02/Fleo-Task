c1222092d57b54cb9e3585a459e5d16f62d390da
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2022-06-04 21:14:48 +0530

    [feature] : Chart added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react";
+import Chart from "./components/Chart";
+import { obj } from "./data";
 function App() {
-  return <div className="text-center my-2">Fleo Task Awesome..</div>;
+  const [data, setData] = useState({});
+  useEffect(() => {
+    setData(obj);
+  }, []);
+  const isDataEmpty = (obj) => Object.keys(obj).length === 0;
+  return (
+    <div className="min-w-screen min-h-screen w-full h-full p-4">
+      <h1 className="text-center text-[2rem] mb-6">Tata Steels</h1>
+      {isDataEmpty(data) ? (
+        <>Loading.....</>
+      ) : (
+        <div className="flex justify-center">
+          <Chart data={data} isRoot={true} />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Chart.js
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from "react";
+
+const Chart = ({ data, isRoot }) => {
+  const [expand, setExpand] = useState(false);
+  const [percent, setPercent] = useState(0);
+  useEffect(() => {
+    setPercent(getPerCent());
+  }, []);
+  const handleClick = () => {
+    setExpand(!expand);
+  };
+  const getPerCent = () => {
+    return parseInt((data.totalSales / data.expectedSales) * 100);
+  };
+  const formatMoney = (number) => {
+    return number.toLocaleString("en-IN", {
+      style: "currency",
+      currency: "INR",
+    });
+  };
+  const getLabel = () => {
+    if (percent <= 33) return "At Risk";
+    else if (percent <= 66) return "Off Track";
+    return "At Track";
+  };
+  const getLabelColor = () => {
+    if (percent <= 33) return "#ff4d4d";
+    else if (percent <= 66) return "#e5e600";
+    return "#94df3a";
+  };
+  const getProgressBarBackgroundColor = () => {
+    if (percent <= 33) return "#ffe5e5";
+    else if (percent <= 66) return "#ffffe5";
+    return "rgb(243, 251, 233)";
+  };
+  const getProgressBarColor = () => {
+    if (percent <= 33) return "#ff4d4d";
+    else if (percent <= 66) return "#ffff4d";
+    return "#94df3a";
+  };
+  return (
+    <div className="grid  gap-3">
+      <div className="w-96 justify-self-center">
+        {isRoot ? "" : <div className=""></div>}
+        <div
+          className="bg-[white] p-4 rounded cursor-pointer"
+          onClick={handleClick}
+        >
+          <div className="flex justify-between items-center mb-6">
+            <h1 className="text-xl">{data.name}</h1>
+            <p className="text-sm">{getPerCent()}% completed</p>
+          </div>
+          <div className="flex justify-between items-center">
+            <div className="">
+              <p className="">Total Sales : {formatMoney(data.totalSales)}</p>
+              <p className="">
+                Target Sales : {formatMoney(data.expectedSales)}
+              </p>
+            </div>
+            <div className="">
+              <div
+                className={` border  px-2 py`}
+                style={{
+                  color: getLabelColor(),
+                  borderColor: getLabelColor(),
+                }}
+              >
+                {getLabel()}
+              </div>
+            </div>
+          </div>
+          <div
+            className={` w-[23rem] h-[1rem] max-w-full rounded-xl mt-4`}
+            style={{
+              background: getProgressBarBackgroundColor(),
+            }}
+          >
+            <div
+              className={` h-[1rem] rounded-2xl `}
+              style={{
+                width: `${percent}%`,
+                background: getProgressBarColor(),
+              }}
+            ></div>
+          </div>
+        </div>
+      </div>
+      {expand ? (
+        <div className="flex gap-2">
+          {data.children.map((child, index) => {
+            return <Chart data={child} key={index} isRoot={false} />;
+          })}
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
+
+export default Chart;
